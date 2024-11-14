cbfe8efbb60ee9dd3a34179f2bd8187cd05a5323
yifan112358 <3206103903@qq.com>
2024-11-14 19:20:53 +0800

    task1

--- Midterm/Task1/main.py
+++ Midterm/Task1/main.py
@@ -1,7 +1,7 @@
 # 你知道嘛？python 的 print 有四种写法！看我给你慢慢写来...
 
 def foo(abab: list):
-    abab.sort()
+    abab.sort()# sort()函数用于对列表中的元素进行排序
 
 
 def bar(woc):
@@ -13,10 +13,10 @@ if __name__ == '__main__':
 
     nums = [1, 2, 3, 4, 5]
     print('init nums >> ', nums)
-    nums.reverse()
+    nums.reverse()# reverse函数用于颠倒元素顺序
     print(f"after reverse >> {nums}")
     foo(nums)
-    print("final res >>", end=' ')
+    print("final res >>", end=' ')# end =' '用于设置打印结束时的字符
     bar(nums)  # Output: [1, 2, 3, 4, 5]
 
 # 小知识点：
